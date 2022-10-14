66601509e679eb52efd5fe2cde35beb198776f3b
Simón Buriticá <simonburi409@gmail.com>
2022-10-14 12:04:25 -0500

    add render functions

--- game_of_life.py
+++ game_of_life.py
@@ -3,8 +3,28 @@ import random
 DEAD = 0
 ALIVE = 1
 
-# randomizer = random.choice([0, 1])
-
 
 def random_state(width, height):
     return [[random.randint(0, 1) for i in range(width)] for i in range(height)]
+
+
+def render(state):
+    height = len(state)
+    for i in range(height):
+        print('|', end='')
+        for number in state[i]:
+            if number == ALIVE:
+                print("#", end='')
+            else:
+                print(' ', end='')
+        print('|')
+
+
+def render_n(state):
+    for row in state:
+        print(*row)
+
+
+# x = random_state(6, 4)
+# render(x)
+# render_n(x)
